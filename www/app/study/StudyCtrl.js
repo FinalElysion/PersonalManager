2839ad09993812662ef1c91d5a16fba8e0cd9697
0o晓月巜 <416311458@qq.com>
2016-11-21 17:29:21 +0800

    version1
    该版本为基本实现功能
    没有使用数据库
    修改数据方法需要修改

--- www/app/study/StudyCtrl.js
+++ www/app/study/StudyCtrl.js
@@ -8,6 +8,8 @@ angular.module('app.controllers')
 
 	$scope.isShowEdit = false;
 
+	$scope.data.editStudy = null;
+
 	//init model
 	$ionicModal.fromTemplateUrl('app/study/model/add-study-model.html', {
 		scope: $scope,
@@ -16,9 +18,31 @@ angular.module('app.controllers')
 		$scope.addStudyModal = modal;
 	});
 
+	$ionicModal.fromTemplateUrl('app/study/model/edit-study-model.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.editStudyModal = modal;
+	});
+
+
 	$scope.showAddStudy = function(){
 		$scope.addStudyModal.show();
 	};
+
+	
+	$scope.addStudy = function(data){
+		StudyService.addStudyData(data);
+
+		$scope.data.newStudy = {title:'',desc:'',date:''};
+		$scope.hideDialog();
+	};
+
+	$scope.hideAddDialog = function() {
+		$scope.addStudyModal.hide();
+		//$scope.addStudyModal.remove();
+	};
+
 
 	$scope.showDelete = function(){
 		$scope.isShowDelete = !$scope.isShowDelete;
@@ -28,16 +52,22 @@ angular.module('app.controllers')
 		$scope.isShowEdit = !$scope.isShowEdit;
 	};
 
-	$scope.addStudy = function(data){
-		StudyService.addStudyData(data);
 
-		$scope.data.newStudy = {title:'',desc:'',date:''};
-		$scope.hideDialog();
+	$scope.showEditStudyDialog = function(item){
+		$scope.data.editStudy = item;
+		$scope.editStudyModal.show();
+		
+	};
+	$scope.saveEditStudy  = function(item){
+	 	$scope.hideEditStudyDialog();
+	}
+	$scope.hideEditStudyDialog = function(item){
+		$scope.editStudyModal.hide();
+		$scope.data.editStudy = null;
 	};
 
-	$scope.hideDialog = function() {
-		$scope.addStudyModal.hide();
-		//$scope.addStudyModal.remove();
+	$scope.deleteStudy = function(item){
+		StudyService.removeCostData(item);
 	};
 
 }]);
